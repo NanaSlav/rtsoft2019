c2a3173c45c9e857782ae61a06ca3f12240a61f1
Anastasia Slavina-Borovskaya <Nastya002@bk.ru>
2019-07-11 17:18:53 +0000

    BackgroundSubtractor is added

--- src.cpp
+++ src.cpp
@@ -1,10 +1,17 @@
-#include<opencv2/opencv.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+
 #include <iostream>
 
 using namespace cv;
 
 int main (int argc, char *argv[]) {
 	namedWindow("Camera", WINDOW_AUTOSIZE);
+	namedWindow("Mask", WINDOW_AUTOSIZE);
+
 	VideoCapture cap;
 	
 	cap.open(argv[1]);
@@ -15,12 +22,28 @@ int main (int argc, char *argv[]) {
 	}
 
 	Mat frame;
+	Mat mask;
+	Mat back;
 
+	Ptr<BackgroundSubtractor> p;
+
+	p = createBackgroundSubtractorMOG2(); 
+	
+
+	int w = frame.cols;
+	int h = frame.rows;
+	
 	while (true) {
 		cap >> frame;
 		if (frame.empty()) break;
 		imshow("Camera", frame);
+
+		p->apply(frame, mask, 0.1);
+		imshow("Mask", mask);
+
 		if ((char) waitKey(33) >= 0 ) break;
+
+		
 	}
 	return 0;
 }
